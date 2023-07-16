b6399108c95824cf6cefeca30f6cc075be40e341
jgraton-7 <jorge.graton.git@gmail.com>
2023-07-16 00:31:31 -0300

    lineChart para lista tomadas

--- aps/src/component/detalheItens/index.js
+++ aps/src/component/detalheItens/index.js
@@ -2,27 +2,77 @@ import { useParams } from 'react-router-dom'
 
 import HookTomadas from '../../hooks'
 
-function DetalhesItem(){
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-  
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Consumo tomadas Ano 2023',
+    },
+  },
+};
+
+
+
+function DetalhesItem(){
   const {id} = useParams();
   
   const  [Tomadas]  = HookTomadas();
-
-  console.log(Tomadas);
+  
+  const tomada = Tomadas[id - 1];
 
 
   
-  const tomada = Tomadas[id - 1];
- 
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October ', 'November ', 'December' ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '2020',
+        data: tomada.consumo,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+      
 
   return (
     <div>
       <p>teste que sera carregado no site</p>
-      <p>Tomada {id}</p>
-      <p>Nome: {tomada.nameTomada}</p>
+      <p>Tomada</p>
+      <Line options={options} data={data} />
+
     </div>
   );
-};
+  };
 
-export default DetalhesItem;
+  export default DetalhesItem;
--- aps/src/hooks/index.js
+++ aps/src/hooks/index.js
@@ -2,14 +2,14 @@ import { useState } from 'react';
 
 function Tomadas() {
     const [Tomadas, setTomadas] = useState([
-        {idTomada: 1, nameTomada: 'Tomada Computador', status:'ativo'},
-        {idTomada: 2, nameTomada: 'Tomada Geladeira', status:'ativo'},
-        {idTomada: 3, nameTomada: 'Tomada Fogão', status:'inativo'},
-        {idTomada: 4, nameTomada: 'Tomada Banheiro', status:'inativo'},
-        {idTomada: 5, nameTomada: 'Tomada TV Sala', status:'ativo'},
-        {idTomada: 6, nameTomada: 'Tomada TV Quarto', status:'ativo'},
-        {idTomada: 7, nameTomada: 'Tomada Alexa', status:'ativo'},
-        {idTomada: 8, nameTomada: 'Tomada Sala', status:'inativo'}
+        {idTomada: 1, nameTomada: 'Tomada Computador', status:'ativo', consumo: [65, 59, 80, 81, 56, 55, 40, 98]},
+        {idTomada: 2, nameTomada: 'Tomada Geladeira', status:'ativo', consumo: [65, 32, 80, 52, 56, 94, 12]},
+        {idTomada: 3, nameTomada: 'Tomada Fogão', status:'inativo', consumo: [65, 9, 42, 28, 32, 94, 42]},
+        {idTomada: 4, nameTomada: 'Tomada Banheiro', status:'inativo', consumo: [65, 14, 34, 81, 36, 97, 40]},
+        {idTomada: 5, nameTomada: 'Tomada TV Sala', status:'ativo', consumo: [65, 59, 80, 73, 24, 55, 40]},
+        {idTomada: 6, nameTomada: 'Tomada TV Quarto', status:'ativo', consumo: [65, 59, 80, 81, 56, 55, 40]},
+        {idTomada: 7, nameTomada: 'Tomada Alexa', status:'ativo', consumo: [65, 59, 80, 81, 56, 55, 40]},
+        {idTomada: 8, nameTomada: 'Tomada Sala', status:'inativo', consumo: [65, 59, 80, 81, 56, 55, 40]}
       ]);
 
   return [Tomadas];
