ee1834f7aa5fea582c157c507a22d05698008fe6
jgraton-7 <jorge.graton.git@gmail.com>
2023-11-08 09:34:01 -0300

    tofixed ajuste

--- nodejs-mysql-webapi/index.js
+++ nodejs-mysql-webapi/index.js
@@ -445,7 +445,7 @@ app.post('/listaConsumoTomada', (req, res) => {
       res.status(500).json({ error: 'Erro ao executar a consulta' });
     }
     else{
-      if(results.length !== 0){
+      if(results.length !== 0 || results !== undefined || results[0].soma !== null){
         Consumototal = results[0].soma.toFixed(2);
         valoraPagar = ((Consumototal) * 0.94).toFixed(2);
       }
